ec42f5d22a230c270ead518b74d5951292b0b692
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-05 17:08:06 +0200

    Update LobbyCommand.java

--- src/main/java/com/example/lobby/LobbyCommand.java
+++ src/main/java/com/example/lobby/LobbyCommand.java
@@ -5,12 +5,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.StandardMessenger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class LobbyCommand extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("LobbyCommand enabled!");
+        // Bungee Channel registrieren
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
     @Override
@@ -23,8 +30,26 @@ public class LobbyCommand extends JavaPlugin {
 
         Player player = (Player) sender;
 
-        player.sendMessage("§aSending you to lobby...");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "server 2 " + player.getName());
+        if (!player.hasPermission("lobby.use")) {
+            player.sendMessage("§cNo permission.");
+            return true;
+        }
+
+        // Zielserver aus Config
+        String targetServer = getConfig().getString("server", "2");
+
+        player.sendMessage(getConfig().getString("message", "§aSending you to lobby..."));
+
+        // BungeeCord Plugin Messaging
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF("Connect");
+            out.writeUTF(targetServer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.sendPluginMessage(this, "BungeeCord", b.toByteArray());
 
         return true;
     }
